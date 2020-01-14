bfb8cb4b5ac4867c2820cd2e4e9cfde637af42af
dezhizhang <1541609448@qq.com>
2020-01-14 19:56:14 +0800

    修改

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,6 @@
 import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
+import { View, Button,Input } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 
 import { add, minus, asyncAdd } from '../../actions/counter'
@@ -54,15 +54,8 @@ interface Index {
 }))
 class Index extends Component {
 
-    /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
     config: Config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: '登录'
   }
 
   componentWillReceiveProps (nextProps) {
@@ -78,21 +71,26 @@ class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+          <View className="header"></View>
+          <View className="content">
+            <View className="box">
+              <View className="list">
+                <Input className="input" type='text' placeholder='请输入手机号' focus/>
+              </View>
+              <View className="list">
+                <Input className="input" type='password' placeholder='请输入手机号' focus/>
+              </View>
+              <View className="wrapper">
+                <View className="left"><Input className="code" placeholder="请输入验证码"/></View>
+                <View className="right"></View>
+                
+              </View>
+            </View>
+          </View>
       </View>
     )
   }
 }
 
-// #region 导出注意
-//
-// 经过上面的声明后需要将导出的 Taro.Component 子类修改为子类本身的 props 属性
-// 这样在使用这个子类时 Ts 才不会提示缺少 JSX 类型参数错误
-//
-// #endregion
 
 export default Index as ComponentClass<PageOwnProps, PageState>
